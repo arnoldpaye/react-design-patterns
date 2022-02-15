06d5ff7bef299b97841a3311fcee6951464fa99c
Arnold Paye <arnoldpayet@gmail.com>
2022-02-15 07:04:58 -0400

    add ResourceLoader component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import { CurrentUserLoader } from './CurrentUserLoader';
-import { UserLoader } from './UserLoader';
+import { ResourceLoader } from './ResourceLoader';
 import { UserInfo } from './UserInfo';
+import { ProductInfo } from './ProductInfo';
 
 function App() {
   return (
     <>
-      <UserLoader userId={'123'}>
+      <ResourceLoader resourceUrl={'/users/123'} resourceName={'user'}>
         <UserInfo />
-      </UserLoader>
-      <UserLoader userId={'234'}>
-        <UserInfo />
-      </UserLoader>
-      <UserLoader userId={'345'}>
-        <UserInfo />
-      </UserLoader>
+      </ResourceLoader>
+      <ResourceLoader resourceUrl={'/products/1234'} resourceName={'product'}>
+        <ProductInfo />
+      </ResourceLoader>
     </>
   );
 }
